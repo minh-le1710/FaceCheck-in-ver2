294cc240f972deaf45357cfe6be97e21a58cd73b
Lê Nhật Minh <nhatlem428@gmail.com>
2025-04-29 11:25:06 +0700

    test

--- face-checkin-2/src/main.cpp
+++ face-checkin-2/src/main.cpp
@@ -1,21 +1,17 @@
 #include <Arduino.h>
 
-// Đổi LED_PIN nếu bạn muốn test pin khác
 #ifndef LED_PIN
-  #define LED_PIN 2    // LED on-board ESP32 DevKit thường là GPIO2
+  #define LED_PIN 2    
 #endif
 
 void setup() {
-  // Thiết lập chân LED là OUTPUT
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // Bật LED
   digitalWrite(LED_PIN, HIGH);
   delay(1900);
 
-  // Tắt LED
   digitalWrite(LED_PIN, LOW);
   delay(10000);
 }
